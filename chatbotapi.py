b2608e47efcb57aa80c7b55e2be31bc8c238460e
VirensinIITBHU <Virendrasinghcharan2605@gmail.com>
2026-05-28 19:28:51 +0530

    added env support

--- chatbotapi.py
+++ chatbotapi.py
@@ -18,6 +18,10 @@ from tenacity import (
 
 load_dotenv()
 
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+if not groq_api_key:
+    raise ValueError("GROQ_API_KEY not found in .env file")
 # ---------------- CLIENT ----------------
 
 client = OpenAI(
